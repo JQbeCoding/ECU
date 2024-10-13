dd030f5a9863efc69867e289910c1de568019edc
JQbeCoding <148560046+JQbeCoding@users.noreply.github.com>
2024-10-13 13:42:51 -0400

    Update student.java

--- student.java
+++ student.java
@@ -15,10 +15,35 @@ interface StudentListInterFace{
     */
     public void searchForStudent();
     /*
+    This method is used to add a new Student. It should have four
+    parameters that represent the id, name, standing, and major. If the id is already in
+    the student roster, then a message should be printed informing the user that the
+    student already exists. This method returns a boolean value. If the student is
+    added, it returns true; otherwise it returns false.
     */
     public void addStudent();
+    /*
+    This method should have an id (of String type) as the parameter.
+    It should remove the Student from the roster if the id is found. Otherwise it should
+    print a message. This method returns a boolean value. If the student is removed, it
+    returns true; otherwise it returns false.
+    */
     public void removeStudent();
+    /*
+    This method should have a major as the parameter. It
+    should return an ArrayList object with all the students with the given major. If
+    there is no student with the given major, the size of the ArrayList will be zero.
+    */
     public void getStudentsByMajor();
+    /*
+    This method has no parameter and returns nothing. It should sort all the
+    students by their id.
+    */
     public void sort();
+    /*
+    This method has no parameter. It should write the student roster back to the
+    file if the data has been changed (the same file is used for both reading and
+    writing).
+    */
     public void save();
 }
